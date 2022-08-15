c5665d150ab218b8ce1c601ed1123421e3703b2b
marcio-breno <mbreno.dev@gmail.com>
2022-08-15 19:47:00 -0300

    Adicionado lista

---
+++ src/components/Box.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="box has-text-weigth-bold">
+    <slot></slot>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Box",
+});
+</script>
+
+<style scoped>
+.box {
+  background: #faf0ca;
+}
+</style>
---
+++ src/components/Tarefa.vue
@@ -0,0 +1,33 @@
+<template>
+  <Box>
+    <div class="columns">
+      <div class="column is-7">
+        {{ tarefa.descricao || "Tarefa sem descrição" }}
+      </div>
+    </div>
+    <div class="column">
+      <Cronometro :tempoEmSegundos="tarefa.duracaoEmSegundos" />
+    </div>
+  </Box>
+</template>
+
+<script lang="ts">
+import ITarefa from "@/interfaces/ITarefa";
+import { defineComponent, PropType } from "vue";
+import Box from "./Box.vue";
+
+import Cronometro from "./Cronometro.vue";
+
+export default defineComponent({
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Tarefa",
+  components: { Cronometro, Box },
+  props: {
+    tarefa: {
+      type: Object as PropType<ITarefa>,
+      required: true,
+    },
+  },
+});
+</script>
+
